e02262ff5eded50002fe77c156d376bca192e7dc
itou-daiki <141300506+itou-daiki@users.noreply.github.com>
2023-08-15 06:46:33 +0000

    進数変換機能を追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,22 @@
+# Streamlitライブラリをインポート
 import streamlit as st
 
-st.set_page_config(page_title="Streamlitテンプレート", layout="wide")
+# タイトルを設定
+st.title('Streamlitのサンプルアプリ')
 
-st.title("Streamlitテンプレート")
+# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
+user_input = st.text_input('あなたの名前を入力してください')
 
-st.caption("StreamlitでWebアプリケーション開発")
+# ボタンを作成し、クリックされたらメッセージを表示
+if st.button('挨拶する'):
+    st.write(f'こんにちは、{user_input}さん!')
 
-st.header("説明")
+# スライダーを作成し、値を選択
+number = st.slider('好きな数字を選んでください', 0, 100)
+
+# 選択した数字を表示
+st.write(f'あなたが選んだ数字は{number}です。')
+
+# 2進数に変換する機能
+binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
+st.write(f'その数字の2進数表現は{binary_representation}です。')
